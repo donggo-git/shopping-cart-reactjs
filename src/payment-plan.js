91e424a991515d0d6a6fd06f37658c47ee021711
Dong <donggnguyen12@gmail.com>
2020-12-25 21:08:18 -0800

    correct

--- src/payment-plan.js
+++ src/payment-plan.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import { GrClose } from 'react-icons/gr'
 class PaymentPlan extends React.Component {
@@ -24,25 +24,7 @@ class PaymentPlan extends React.Component {
             </div>)
     }
 }
-=======
-import React from 'react';
-class PaymentPlan extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        let total = this.props.total;
-        let PaymentPlantFirst = { pay: Math.round(total / 12 * 100) / 100, howLong: 12 };
-        let PaymentPlanSecond = { pay: 20, howLong: Math.round(total / 20) };
-        return (
-            <div>
-                <h3>Your total is {total}</h3>
-                <ul>
-                    <li>first payment plan: ${PaymentPlantFirst.pay}/month for {PaymentPlantFirst.howLong} months</li>
-                    <li>first payment plan: ${PaymentPlanSecond.pay}/month for {PaymentPlanSecond.howLong} months</li>
-                </ul>
-            </div>)
-    }
-}
->>>>>>> 8173c304de958619f2c78f4005ab6eff537db75f
+
+
+
 export default PaymentPlan
